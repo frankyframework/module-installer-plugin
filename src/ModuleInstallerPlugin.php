f386cfb550ab50470b7214fc7ee0b43c4ecf4d3b
frankyframework <49538152+frankyframework@users.noreply.github.com>
2021-06-29 12:45:11 -0500

    Create ModuleInstallerPlugin.php

---
+++ src/ModuleInstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Franky\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class ModuleInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new ModuleInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
